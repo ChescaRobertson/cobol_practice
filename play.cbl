a0f58af98f50ced3003a9b9e3a8e570509ddacfa
Francesca Robertson <58787784+ChescaRobertson@users.noreply.github.com>
2021-05-27 14:37:04 +0100

    Folder Formatting

--- play.cbl
+++
@@ -1,4 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. two.
-       PROCEDURE DIVISION.
-           DISPLAY "No don't listen to her, I'm valid!".
--- sub-program.cbl
+++
@@ -1,5 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. sub-program.
-       PROCEDURE DIVISION.
-           DISPLAY "Thank you main-program!".
-           DISPLAY "I am the sub-program.".
