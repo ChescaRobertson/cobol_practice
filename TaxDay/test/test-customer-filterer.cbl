69628c59a58cc8b2803f3fa33d0c1ac5a206ac0c
Francesca Robertson <58787784+ChescaRobertson@users.noreply.github.com>
2021-06-01 12:24:01 +0100

    Taxday and tests fully working

--- TaxDay/test/test-customer-filterer.cbl
+++ TaxDay/test/test-customer-filterer.cbl
@@ -3,8 +3,8 @@
        PROCEDURE DIVISION.
            CALL "customer-filterer" USING "03-05", "2021".
 
-          *>  SET ENVIRONMENT "cards_dat" TO "cards-birthday-0101.dat".
-          *>  CALL "customer-filterer" USING "01-01", "2021".
-          *>  SET ENVIRONMENT "cards_dat" TO "cards-tax-day.dat".
-          *>  CALL "customer-filterer" USING "04-06", "2021".
+           SET ENVIRONMENT "cards_dat" TO "cards-birthday-0101.dat".
+           CALL "customer-filterer" USING "01-01", "2021".
+           SET ENVIRONMENT "cards_dat" TO "cards-tax-day.dat".
+           CALL "customer-filterer" USING "04-06", "2021".
            
